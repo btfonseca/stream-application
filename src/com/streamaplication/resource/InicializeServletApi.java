b07ffd2bbb30d578bc44f85f9fcbfcfd34def9bd
Bruno fonseca <bruno.fonseca@amazonitec.com>
2022-07-16 09:23:19 -0300

    Configuração de Servlet.

---
+++ src/com/streamaplication/resource/InicializeServletApi.java
@@ -0,0 +1,21 @@
+package com.streamaplication.resource;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InicializeServletApi extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+        writer.println("<h1>Hello, World!</h1>");
+	}  
+	
+}
